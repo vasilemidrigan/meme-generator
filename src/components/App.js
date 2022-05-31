ea97724cc69c50963eb376b6a677aab0b9be163e
Vasile Midrigan <111rigan111@gmail.com>
2022-05-31 04:29:37 +0100

    Add Meme Component and styles to it

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Meme from "./Meme";
 
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+export default function Meme() {
+  return (
+    <div className="meme">
+      <form className="meme__form" action="">
+        <input className="meme__form__input" type="text" />
+        <input className="meme__form__input" type="text" />
+        <button className="meme__form__btn" type="submit">
+          Get a new meme image 🖼
+        </button>
+      </form>
+    </div>
+  );
+}
