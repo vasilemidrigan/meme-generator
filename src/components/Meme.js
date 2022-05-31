d308ad3783166226c8a04d3e8732b9f1b07a766a
Vasile Midrigan <111rigan111@gmail.com>
2022-05-31 04:34:30 +0100

    Add placeholders to input fields and styles to them

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,8 +2,16 @@ export default function Meme() {
   return (
     <div className="meme">
       <form className="meme__form" action="">
-        <input className="meme__form__input" type="text" />
-        <input className="meme__form__input" type="text" />
+        <input
+          className="meme__form__input"
+          type="text"
+          placeholder="top text"
+        />
+        <input
+          className="meme__form__input"
+          type="text"
+          placeholder="bottom text"
+        />
         <button className="meme__form__btn" type="submit">
           Get a new meme image 🖼
         </button>
